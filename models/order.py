141f926fd8c6fa50dac7ce60d1589a33a706ca63
reihanSetyaAbida <reihan.seta.abida@hashmicro.com>
2022-03-16 15:39:34 +0700

    up

--- models/order.py
+++ models/order.py
@@ -1,3 +1,4 @@
+from base64 import encode
 from odoo import api, fields, models
 
 
@@ -8,16 +9,52 @@ class Order(models.Model):
     name = fields.Char(string='Kode Order', required=True)
 
     orderdetail_ids = fields.One2many(
-        comodel_name='wedding.order_detail', inverse_name='order_id', string='Order Detail')
+        comodel_name='wedding.order_detail',
+        inverse_name='order_id',
+        string='Order Detail')
+
+    total = fields.Integer(
+        compute="_compute_total",
+        string='Total Harga',
+        store=True)
+
+    @api.depends('orderdetail_ids')
+    def _compute_total(self):
+        for record in self:
+            a = sum(self.env['wedding.order_detail'].search(
+                [('order_id', '=', record.id)]).mapped('harga'))
+            record.total = a
 
 
 class OrderDetail(models.Model):
     _name = 'wedding.order_detail'
     _description = 'New Description'
 
-    order_id = fields.Many2one(comodel_name='wedding.order', string='Order')
+    order_id = fields.Many2one(
+        comodel_name='wedding.order',
+        string='Order')
     panggung_id = fields.Many2one(
-        comodel_name='wedding.panggung', string='Panggung')
+        comodel_name='wedding.panggung',
+        string='Panggung')
 
-    name = fields.Selection(string='Name', selection=[(
-        'panggung', 'Panggung'), ('kursi tamu', 'Kursi Tamu'), ])
+    name = fields.Selection(
+        string='Name',
+        selection=[
+            ('panggung', 'Panggung'),
+            ('kursi tamu', 'Kursi Tamu'), ])
+    harga = fields.Integer(
+        compute='_compute_harga',
+        string='harga')
+    qty = fields.Integer(string='QTY')
+    harga_satuan = fields.Integer(
+        compute='_compute_harga_satuan', string='Harga Satuan')
+
+    @api.depends('panggung_id')
+    def _compute_harga_satuan(self):
+        for record in self:
+            record.harga_satuan = record.panggung_id.harga
+
+    @api.depends('qty', 'panggung_id')
+    def _compute_harga(self):
+        for record in self:
+            record.harga = record.harga_satuan * record.qty
